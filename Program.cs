5d2d9843011e2cb8fa9b690ce921d33408693812
GINSENG-A7 <zhenya_bykov_1998@mail.ru>
2021-09-23 19:21:18 +0300

    Last_try

--- Program.cs
+++ Program.cs
@@ -1,63 +1,32 @@
 ﻿using System;
-using NUnit.Framework;
 
 namespace TestingLab1
 {
-    [TestFixture]
-    class FigureTest
-    {
-        Rectangle myRect = new Rectangle(7, 10);
-        EquilateralTriangle myTriangle = new EquilateralTriangle(6);
-        Circle myCircle = new Circle(4);
-        Circle myCircle2 = new Circle(5);
-        [Test]
-        public void RectangleTestNo_1()
-        {
-            Assert.AreEqual(70, myRect.Surface());
-        }
-        [Test]
-        public void RectangleTestNo_2()
-        {
-            Assert.AreEqual(48, myRect.Perimeter());
-        }
-        [Test]
-        public void RectangleTestNo_3()
-        {
-            Assert.IsFalse(120 < myRect.Perimeter() + myRect.Surface()); //not valid
-        }
-
-        [Test]
-        public void TriangleTestNo_1()
-        {
-            Assert.IsNotNull(myTriangle.Surface());
-        }
-        [Test]
-        public void TriangleTestNo_2()
-        {
-            Assert.IsNotNull(myTriangle.Perimeter());
-        }
-        [Test]
-        public void TriangleTestNo_3()
-        {
-            Assert.IsNotNull(myTriangle);  //not valid
-        }
-
-        [Test]
-        public void CircleTestNo_1()
-        {
-            Assert.AreNotSame(myCircle.Surface(), myCircle.Perimeter());
-        }
-        [Test]
-        public void CircleTestNo_2()
-        {
-            Assert.AreNotSame(myCircle, myCircle2); //not valid
-        }
-        [Test]
-        public void CircleTestNo_3()
-        {
-            Assert.IsTrue(myCircle.Surface() > myCircle.Perimeter());
-        }
-    }
+    //class Program
+    //{
+        //static void Main(string[] args)
+        //{
+        //    try
+        //    {
+        //        Rectangle myRect = new Rectangle(7, 10);
+        //        myRect.getName();
+        //        Console.WriteLine("Площадь прямоугольника равна {0} ", myRect.Surface());
+        //        Console.WriteLine("Периметр прямоугоьника равен {0} ", myRect.Perimeter());
+        //        EquilateralTriangle myTriangle = new EquilateralTriangle(6);
+        //        myTriangle.getName();
+        //        Console.WriteLine("Площадь равностороннего треугоьника равна {0} ", myTriangle.Surface());
+        //        Console.WriteLine("Периметр равностороннего треугольника равен {0} ", myTriangle.Perimeter());
+        //        Circle myCircle = new Circle(4);
+        //        myCircle.getName();
+        //        Console.WriteLine("Площадь круга равна {0} ", myCircle.Surface());
+        //        Console.WriteLine("Периметр круга равен {0} ", myCircle.Perimeter());
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e.Message);
+        //    }
+        //}
+    //}
 
     abstract class Figure
     {
